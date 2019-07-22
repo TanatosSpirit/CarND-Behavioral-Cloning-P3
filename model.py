0d0d0733fea070126dc1ac04370b90e6c7db754c
Andrey <lytovandrei@gmail.com>
2019-07-23 01:20:10 +0300

    update writeup

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import csv
 import cv2
 import numpy as np
 
+# Loading dataset
 lines = []
 header = True
 
@@ -21,9 +22,9 @@ measurements = []
 correction = 0.3  # this is a parameter to tune
 
 for line in lines:
-    center_image_filename = line[0]
-    left_image_filename = line[1]
-    right_image_filename = line[2]
+    center_image_filename = line[0]  # .split('/')[-1]
+    left_image_filename = line[1]  # .split('/')[-1]
+    right_image_filename = line[2]  # .split('/')[-1]
 
     steering_center = float(line[3])
 
@@ -32,25 +33,32 @@ for line in lines:
     steering_right = steering_center - correction
 
     # read in images from center, left and right cameras
-    path_image = path + "IMG//"  # path to your training IMG directory
-    img_center = cv2.imread(center_image_filename)
-    img_left = cv2.imread(left_image_filename)
-    img_right = cv2.imread(right_image_filename)
+    # path_image = path + "IMG//"  # path to your training IMG directory
+    img_center = cv2.imread(center_image_filename)  # path_image+center_image_filename)
+    img_left = cv2.imread(left_image_filename)   # path_image+left_image_filename)
+    img_right = cv2.imread(right_image_filename)  # path_image+right_image_filename)
 
     images.extend([img_center, img_left, img_right])
     measurements.extend([steering_center, steering_left, steering_right])
+
+# augmented_images, augmented_measurements = [], []
+# for image, measurement in zip(images, measurements):
+#     augmented_images.append(image)
+#     augmented_measurements.append(measurement)
+#     augmented_images.append(cv2.flip(image, 1))
+#     augmented_measurements.append(measurement * -1)
 
 X_train = np.array(images)
 y_train = np.array(measurements)
 
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
 from  keras.layers.convolutional import Convolution2D
 
+#create model NvidiaNet
 model = Sequential()
 
-# NvidiaNet
 model.add(Cropping2D(cropping=((66, 26), (0, 0)), input_shape=(160, 320, 3)))
 model.add(Lambda(lambda x: x / 255.0 - 0.5))
 model.add(Convolution2D(filters=24, kernel_size=5, strides=(2, 2), activation='relu'))
@@ -65,6 +73,10 @@ model.add(Dense(10))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
+
+#Training
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=20)
+
+model.summary()
 
 model.save('model.h5')
